60aed6c79997b76f16e78fd88e1d971030621849
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-22 21:44:57 -0300

    Finished Pre-rendering and Data Fetching

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,15 @@ import Link from 'next/link'
 
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 
-export default function Home() {
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
+      {/* Introduction */}
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Pedro. I'm a software engineer from Brazil.</p>
         <p>
@@ -17,6 +19,38 @@ export default function Home() {
           <Link href="https://github.com/barcellos-pedro"> Github 🐙</Link>
         </p>
       </section>
+
+      {/* Posts List */}
+      {!allPostsData?.length > 0 ? (
+        <h2>No posts yet.</h2>
+      ) : (
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Posts</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
     </Layout>
   )
+}
+
+/**
+ * Get data at build time and then send as props to Home component
+ */
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData,
+    },
+  }
 }
