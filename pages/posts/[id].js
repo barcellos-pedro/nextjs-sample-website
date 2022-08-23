a5fb4933b61052b27ccfa3d5f81a07cc2e576ca8
Pedro Reis <pedrobarcellosdosreis@gmail.com>
2022-08-23 15:06:50 -0300

    Render markdown content and style Post page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
+
+import Date from '../../components/date'
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
+import utilStyles from '../../styles/utils.module.css'
 
 export default function Post({ postData }) {
   return (
@@ -8,11 +11,13 @@ export default function Post({ postData }) {
       <Head>
         <title>{postData.title}</title>
       </Head>
-      {postData.title}
-      <br />
-      {postData.id}
-      <br />
-      {postData.date}
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
     </Layout>
   )
 }
@@ -34,7 +39,7 @@ export async function getStaticPaths() {
  * Fetches data for the post with and given [id]
  */
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData,
